faf4df20dd49c5b8136247ece807153865724995
Nandini-KV <parnapallinandini2@gmail.com>
2024-08-12 16:28:38 +0530

    Initial commit

---
+++ CloneRepo/src/main/java/com/clone/CloneRepo/CloneRepoApplication.java
@@ -0,0 +1,13 @@
+package com.clone.CloneRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneRepoApplication.class, args);
+	}
+
+}
---
+++ CloneRepo/src/test/java/com/clone/CloneRepo/CloneRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.CloneRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
